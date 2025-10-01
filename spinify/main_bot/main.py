a5dedd695d6395ffb334994a1bd977ba6018f16d
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-10-01 14:03:21 +0530

    Update main.py

--- spinify/main_bot/main.py
+++ spinify/main_bot/main.py
@@ -1,14 +1,19 @@
-import asyncio, os
+import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+
 from ..common.config import MAIN_BOT_TOKEN
+from ..common.db import init_core, init_bot_tables
 from .menu import router as menu_router
 from .gate import router as gate_router
-from ..common.db import init_core, init_bot_tables
 
 async def main():
     init_core(); init_bot_tables()
-    bot = Bot(MAIN_BOT_TOKEN, parse_mode=ParseMode.HTML)
+    bot = Bot(
+        MAIN_BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
     dp = Dispatcher()
     dp.include_router(gate_router)
     dp.include_router(menu_router)
